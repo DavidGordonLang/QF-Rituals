715ee3fc7248519ca685684a0582eb75ab31bd55
DavidGordonLang <davidgordonlang@gmail.com>
2025-08-27 14:06:49 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/tailwind.css";
+import "./styles/tailwind.css";        // <-- make sure this import exists
 import { App } from "./app/App";
 
 createRoot(document.getElementById("root")!).render(
