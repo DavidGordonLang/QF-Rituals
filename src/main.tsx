dc3d75553a392abe5f3c6304871315069c507433
DavidGordonLang <davidgordonlang@gmail.com>
2025-09-06 19:04:15 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App";              // ← default import (App is default export)
+import App from "./app/App";
 import "./styles/tailwind.css";
+import "./suiteBus.ts"; // <-- Suite Bridge
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
